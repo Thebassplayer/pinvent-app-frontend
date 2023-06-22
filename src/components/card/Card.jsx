ca7f0ce4177022913ec6418fd14827741abdcc8e
Roy Lopez <roylopezdev@gmail.com>
2023-06-22 18:49:37 -0300

    create login page

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Card.module.css";
+import styles from "./Card.module.scss";
 
 const Card = ({ children, cardClass }) => {
   return <div className={`${styles.card} ${cardClass}`}>{children}</div>;
