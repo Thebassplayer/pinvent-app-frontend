1701bd83281c1c889412b80e2c8780a3030701d2
Roy Lopez <roylopezdev@gmail.com>
2023-06-24 19:35:41 -0300

    Sidebar auto width functionality

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { useState } from "react";
+
+// React Icons
+import { HiMenuAlt3 } from "react-icons/hi";
+import { RiProductHuntLine } from "react-icons/ri";
+
+// Components
+import menu from "../../data/sidebar";
+
+// Styles
 import "./Sidebar.scss";
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <div className="layout">
-      <div className="sidebar">
-        <h2>Sidebar</h2>
+      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }}>
+        <div className="top_section">
+          <div
+            className="logo"
+            style={{
+              display: isOpen ? "block" : "none",
+            }}
+          >
+            <RiProductHuntLine size={35} style={{ cursor: "pointer" }} />
+          </div>
+          <div
+            className="bars"
+            style={{
+              marginLeft: isOpen ? "100px" : "0px",
+            }}
+          >
+            <HiMenuAlt3 onClick={toggle} />
+          </div>
+        </div>
       </div>
       <main>{children}</main>
     </div>
