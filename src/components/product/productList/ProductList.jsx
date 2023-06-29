1405ea2b198974f91963cef7882e03316a166095
Roy Lopez <roylopezdev@gmail.com>
2023-06-29 11:46:54 -0300

    Create Product List Component

---
+++ src/components/product/productList/ProductList.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+// Components
+import { SpinnerImg } from "../../loader/Loader";
+// Utilitys
+import nameShorter from "../../../utils/nameShorter.js";
+//Styles
+import "./ProductList.scss";
+// Icons
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { AiOutlineEye } from "react-icons/ai";
+
+const ProductList = ({ products, isLoading }) => {
+  return (
+    <div className="product-list">
+      <hr />
+      <div className="table">
+        <div className="--flex-between --flex-dir-column">
+          <span>
+            <h3>Inventory Item</h3>
+            <span>
+              <h3>Search Products</h3>
+            </span>
+          </span>
+        </div>
+        {isLoading && <SpinnerImg />}
+        <div className="table">
+          {!isLoading && products.length === 0 ? (
+            <p>No product found, please add a product</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Value</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product, index) => {
+                  const { _id, name, category, price, quantity } = product;
+                  return (
+                    <tr key={_id}>
+                      <td>{index + 1}</td>
+                      <td>{nameShorter(name, 18)}</td>
+                      <td>{category}</td>
+                      <td>{`$${price}`}</td>
+                      <td>{quantity}</td>
+                      <td>{`$${price * quantity}`}</td>
+                      <td className="icons">
+                        <span>
+                          <AiOutlineEye size={25} color={"purple"} />
+                        </span>
+                        <span>
+                          <FaEdit size={20} color={"green"} />
+                        </span>
+                        <span>
+                          <FaTrashAlt size={20} color={"red"} />
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,37 @@
+import { useEffect } from "react";
+// Custom Hooks
 import useRedirectLogedOutUser from "../../Hooks/useRedirectLogedOutUser";
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+//Redux Slices
+import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
+import { getProducts } from "../../redux/features/product/productSlice";
+//Components
+import ProductList from "../../components/product/productList/ProductList";
 
 const Dashboard = () => {
   useRedirectLogedOutUser("login");
+
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const { products, isLoading, isError, message } = useSelector(
+    state => state.product
+  );
+
+  useEffect(() => {
+    if (isLoggedIn === true) {
+      dispatch(getProducts());
+    }
+
+    if (isError) {
+      console.log(message);
+    }
+  }, [isLoading, isError, message]);
   return (
     <div>
       <h2>Dashboard</h2>
+      <ProductList products={products} isLoading={isLoading} />
     </div>
   );
 };
