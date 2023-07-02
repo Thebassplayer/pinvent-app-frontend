7f0b3c3c1c2fbda51964747463f5af19ac650c9a
Roy Lopez <roylopezdev@gmail.com>
2023-07-02 11:35:01 -0300

    Create Profile component

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+//Components
+import { SpinnerImg } from "../../components/loader/Loader";
+import Card from "../../components/card/Card";
 // Redux
 import { useSelector, useDispatch } from "react-redux";
 import { getUser } from "../../services/authServices";
@@ -7,6 +10,7 @@ import { SET_NAME, SET_USER } from "../../redux/features/auth/authSlice";
 import useRedirectLoggedOutUser from "../../Hooks/useRedirectLoggedOutUser";
 // Styles
 import "./Profile.scss";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   useRedirectLoggedOutUser("/login");
@@ -18,14 +22,53 @@ const Profile = () => {
     setIsLoading(true);
     const getProfile = async () => {
       const data = await getUser();
+      console.log("Data: ", data);
       setProfile(data);
       setIsLoading(false);
       await dispatch(SET_USER(data));
-      await dispatch(SET_NAME(data.name));
+      await dispatch(SET_NAME(data.username));
     };
     getProfile();
   }, [dispatch]);
-  return <div>Profile</div>;
+  return (
+    <div className="profile --my2">
+      {isLoading && <SpinnerImg />}
+      <>
+        {!isLoading && profile === null ? (
+          <p>Something whent wrong, reload page...</p>
+        ) : (
+          <Card cardClass={"card --flex-dir-column"}>
+            <span className="profile photo">
+              <img src={profile?.photo} alt="profilepic" />
+            </span>
+            <span className="profile-data">
+              <p>
+                <b>Name : </b>
+                {profile?.username}
+              </p>
+              <p>
+                <b>Email : </b>
+                {profile?.email}
+              </p>
+              <p>
+                <b>Phone : </b>
+                {profile?.phone}
+              </p>
+              <p>
+                <b>Bio : </b>
+                {profile?.bio}
+              </p>
+              <div>
+                <Link to="edit/profile">
+                  <button className="--btn --btn-primary">Edit Profile</button>
+                </Link>
+              </div>
+            </span>
+          </Card>
+        )}
+      </>
+    </div>
+  );
 };
 
 export default Profile;
